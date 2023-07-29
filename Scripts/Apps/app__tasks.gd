afabc8d3bdf271af3ae61c974df642a3ba25013f
TripleCubes <113813506+TripleCubes@users.noreply.github.com>
2023-07-29 20:35:39 +0700

    Add more tasks

--- Scripts/Apps/app__tasks.gd
+++ Scripts/Apps/app__tasks.gd
@@ -11,22 +11,55 @@ func _init(window: Game_Window):
 	_window._min_h = 170
 	_window._max_h = 170
 	
-	_task_10_flowers = Game_Task.new(15, preload("res://Assets/Sprites/Apps/Tasks/app__tasks__ram.png"),
+	_task_list.append(Game_Task.new(15, preload("res://Assets/Sprites/Apps/Tasks/app__tasks__30_watering_cans.png"),
+						"fill 30 watering cans", func sub_text() -> String:
+		return str(10 - Stats.flowers_shipped) + " more"
+	, _window._scale, func task() -> bool:
+		return Stats.flowers_shipped >= 10
+	))
+	add_child(_task_list[0])
+	
+	_task_list.append(Game_Task.new(15 + 30, preload("res://Assets/Sprites/Apps/Tasks/app__tasks__10_flowers.png"),
 						"Ship 10 flowers", func sub_text() -> String:
 		return str(10 - Stats.flowers_shipped) + " more"
 	, _window._scale, func task() -> bool:
-		return true
-	)
-	add_child(_task_10_flowers)
+		return Stats.flowers_shipped >= 10
+	))
+	add_child(_task_list[1])
+	
+	_task_list.append(Game_Task.new(15 + 30 * 2, preload("res://Assets/Sprites/Apps/Tasks/app__tasks__grow_big_tree.png"),
+						"Grow big tree", func sub_text() -> String:
+		return "Grow big tree"
+	, _window._scale, func task() -> bool:
+		return Stats.flowers_shipped >= 10
+	))
+	add_child(_task_list[2])
+	
+	_task_list.append(Game_Task.new(15 + 30 * 3, preload("res://Assets/Sprites/Apps/Tasks/app__tasks__3_dead_flowers.png"),
+						"3 dead flowers", func sub_text() -> String:
+		return ""
+	, _window._scale, func task() -> bool:
+		return Stats.flowers_shipped >= 10
+	))
+	add_child(_task_list[3])
+	
+	_task_list.append(Game_Task.new(15 + 30 * 4, preload("res://Assets/Sprites/Apps/Tasks/app__tasks__done_all.png"),
+						"Finish all tasks", func sub_text() -> String:
+		return ""
+	, _window._scale, func task() -> bool:
+		return Stats.flowers_shipped >= 10
+	))
+	add_child(_task_list[4])
 	
 func draw_app_content() -> void:
 	pass
 	
 func update(_delta: float) -> void:
-	_task_10_flowers.update(_delta)
+	for task in _task_list:
+		task.update(_delta)
 	
 #@onready var _window_list: Node2D = get_node("/root/Main/WindowList")
 #const _texture__: Texture2D = preload("res://Assets/Sprites/Apps/")
 var _window: Game_Window
 
-var _task_10_flowers: Game_Task
+var _task_list: = []
--- Scripts/Game/task.gd
+++ Scripts/Game/task.gd
@@ -1,14 +1,12 @@
 class_name Game_Task
 extends Node2D
 
-const _texture_finished: Texture2D = preload("res://Assets/Sprites/Apps/Tasks/app__tasks__finished.png")
-
 var _icon: Texture2D
 var _y: float = 0
 var _text: String
 var _sub_text: String
 var _label: RichTextLabel
-var _label_tasks: RichTextLabel
+var _label_sub_text: RichTextLabel
 var _task_func: Callable
 var _sub_text_func
 
@@ -24,32 +22,30 @@ func _init(y: float, icon: Texture2D, text: String, sub_text_func: Callable,
 	_task_func = task_func
 	
 	_label = RichTextLabel.new()
-	GlobalFunctions.setup_label(_label, 60, y + 10, text)
+	GlobalFunctions.setup_label(_label, 60, y * 2 - 4, text)
 	add_child(_label)
 	
-	_label_tasks = RichTextLabel.new()
-	GlobalFunctions.setup_label(_label_tasks, 60, y + 35)
-	add_child(_label_tasks)
+	_label_sub_text = RichTextLabel.new()
+	GlobalFunctions.setup_label(_label_sub_text, 60, y * 2 + 19)
+	add_child(_label_sub_text)
 	
 func _draw():
 	draw_set_transform(Vector2(0, 0), 0, Vector2(_scale_ref.get_var(), _scale_ref.get_var()))
-	if _task_func.call():
-		draw_texture(_texture_finished, Vector2(190/2, _y + 3))
 	draw_texture(_icon, Vector2(5, _y))
 	
 func update(_delta):
 	if _task_func.call():
-		_label_tasks.text = "Done"
+		_label_sub_text.text = "Done"
 	else:
-		_label_tasks.text = _sub_text_func.call()
+		_label_sub_text.text = _sub_text_func.call()
 	
 	if _scale_ref.transitioning():
 		_label.scale = Vector2(_scale_ref.get_var(), _scale_ref.get_var())
 		_label.position.x = 60 * _scale_ref.get_var()/2
-		_label.position.y = (_y+10) * _scale_ref.get_var()/2
+		_label.position.y = (_y*2 - 4) * _scale_ref.get_var()/2
 		
-		_label_tasks.scale = Vector2(_scale_ref.get_var(), _scale_ref.get_var())
-		_label_tasks.position.x = 60 * _scale_ref.get_var()/2
-		_label_tasks.position.y = (_y+35) * _scale_ref.get_var()/2
+		_label_sub_text.scale = Vector2(_scale_ref.get_var(), _scale_ref.get_var())
+		_label_sub_text.position.x = 60 * _scale_ref.get_var()/2
+		_label_sub_text.position.y = (_y*2+19) * _scale_ref.get_var()/2
 	
 	queue_redraw()
